8add055c132d53be0888efe00c00aa516024d406
Jesse Smith <jesseholden713@gmail.com>
2026-01-28 20:36:13 -0800

    removed strapi plugin now that we're using resend

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,16 +1,4 @@
 export default ({ env }) => ({
-  email: {
-    config: {
-      provider: 'resend',
-      providerOptions: {
-        apiKey: env('RESEND_API_KEY'),
-      },
-      settings: {
-        defaultFrom: env('EMAIL_DEFAULT_FROM', 'noreply@ledillumination.com'),
-        defaultReplyTo: env('EMAIL_DEFAULT_REPLY_TO', 'info@ledillumination.com'),
-      },
-    },
-  },
   'users-permissions': {
     config: {
       jwtSecret: env('JWT_SECRET'),
